4fab397aa86b60fcdc82da6bd1a45c46d6735716
Espen Ekvang <espen.ekvang@bekk.no>
2013-03-25 11:22:44 +0100

    Initial commit

---
+++ PersonTableStorage/PersonTableStorage.WebRole/Controllers/CompanyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using PersonTableStorage.WebRole.Models;
+
+namespace PersonTableStorage.WebRole.Controllers
+{
+    public class CompanyController : Controller
+    {
+        //
+        // GET: /Person/
+
+        public ActionResult Index()
+        {
+            var persons = new List<Person>
+                              {
+                                  new Person
+                                      {
+                                          FirstName = "Albert",
+                                          LastName = "Einstein",
+                                          Email = "albert@einstein.com"
+                                      },
+                                  new Person
+                                      {
+                                          FirstName = "Isaac",
+                                          LastName = "Newton",
+                                          Email = "isaac@newton.com"
+                                      }
+                              };
+            var company = new Company { Persons = persons };
+            return View(company);
+        }
+    }
+}
---
+++ PersonTableStorage/PersonTableStorage.WebRole/Models/Company.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using PersonTableStorage.WebRole.Controllers;
+
+namespace PersonTableStorage.WebRole.Models
+{
+    public class Company
+    {
+        public IEnumerable<Person> Persons { get; set; }
+    }
+}
