5b4305887b76a28af29550b5844ced1fe66766f0
Espen Ekvang <espen.ekvang@bekk.no>
2013-03-25 12:35:00 +0100

    Added initial solution for PersonSQLDatabase

---
+++ PersonSQLDatabase/PersonSQLDatabase.WebRole/Models/Company.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PersonSQLDatabase.WebRole.Models
+{
+    public class Company
+    {
+        public IEnumerable<Person> Persons { get; set; }
+    }
+}
